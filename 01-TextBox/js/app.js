f7ab7ac4925f39959bb00fed825483ad55a75049
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-13 14:51:49 -0300

    criação app javascript com exemplo de TextBox.

---
+++ 01-TextBox/js/app.js
@@ -0,0 +1,13 @@
+document.getElementById('btnSubmit').onclick = function() {
+
+    var txtAutor = document.getElementById('autor')
+    var txtEmail = document.getElementById('email')
+    var txtSenha = document.getElementById('senha')
+    var txtComentarios = document.getElementById('texto')
+
+    console.log(txtAutor.value);
+    console.log(txtEmail.value);
+    console.log(txtSenha.value);
+    console.log(txtComentarios.value);
+
+}
