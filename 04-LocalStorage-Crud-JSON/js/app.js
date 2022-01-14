017655c82a2513b6e7b1460126dc4a273fd26d9d
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-14 09:41:13 -0300

    criação dos componentes de exclusão e edição.

--- 04-LocalStorage-Crud-JSON/js/app.js
+++ 04-LocalStorage-Crud-JSON/js/app.js
@@ -30,7 +30,7 @@ function adicionarOuAlterar(e) {
         adicionar(p)
 
     else if (idAlterar > 0)
-        alert('Alterar')
+        alterar(p)
 
     else
         alert('Ação desconhecida')
@@ -41,9 +41,9 @@ function adicionar(p) {
     var idValido = 1;
 
     // Verificar item no localstorage
-    if (localStorage.getItem('value') !== null) {
+    if (localStorage.getItem('pessoa') !== null) {
         // Busca array objetos JSON
-        pessoas = JSON.parse(localStorage.getItem('value'))
+        pessoas = JSON.parse(localStorage.getItem('pessoa'))
 
         if (pessoas.length > 0)
             idValido = (function obterIdValido() {
@@ -74,7 +74,7 @@ function adicionar(p) {
     })
 
     // Armazenando no LocalStorage
-    localStorage.setItem('value', JSON.stringify(pessoas))
+    localStorage.setItem('pessoa', JSON.stringify(pessoas))
 
     // Redefine os campos do formulário
     document.getElementById('frmCadastro').reset()
@@ -83,11 +83,11 @@ function adicionar(p) {
 
 //  BLOCO LISTAR
 function listar() {
-    if (localStorage.getItem('value') == null)
+    if (localStorage.getItem('pessoa') == null)
 
         return
 
-    var pessoas = JSON.parse(localStorage.getItem('value'))
+    var pessoas = JSON.parse(localStorage.getItem('pessoa'))
     var tbody = document.getElementById('tbodyResultados')
 
     tbody.innerHTML = ''
@@ -107,7 +107,90 @@ function listar() {
             '<td>' + sexo + '</td>' +
             '<td>' + data + '</td>' +
             '<td><button onclick="excluir(\'' + id + '\')">Excluir</button></td>' +
-            '<td><button onclick="preparaAlterar(\'' + id + '\')">Alterar</button></td>' +
+            '<td><button onclick="prepararAlterar(\'' + id + '\')">Alterar</button></td>' +
             '</tr>'
     }
+}
+
+// BLOCO ALTERAR
+function prepararAlterar(idRow) {
+    document.getElementById('btnCadastrarSalvar').value = 'Salvar'
+
+    var txtNome = document.getElementById('txtNome'),
+        dtpDataNascimento = document.getElementById('dtpDataNascimento'),
+        rdoMasculino = document.getElementById('rdoMasculino'),
+        rdoFeminino = document.getElementById('rdoFeminino')
+
+    var pessoas = JSON.parse(localStorage.getItem('pessoa'))
+
+    for (var index = 0; index < pessoas.length; index++) {
+        if (pessoas[index].Id == idRow) {
+            // popular os campos 
+            txtNome.value = pessoas[index].Nome
+                // //caso fosse tipo date toISOString().substring(0, 10);
+            dtpDataNascimento.value = pessoas[index].DataNascimento.replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3-$2-$1')
+            rdoMasculino.checked = !(rdoFeminino.checked = (pessoas[index].Sexo == 'F'))
+
+            // recarrega a lista para limpar o th com background alterado
+            listar()
+
+            // coloco ID null (caso clicar em mais de um elemento no botao alterar)
+            idAlterar = null
+
+            if (idAlterar === null) {
+                // muda o background da nova linha
+                var th = document.getElementById('rowTable' + index)
+                th.className = 'estadoAlteracao'
+            }
+
+            // atribuir o Id a variavel global
+            idAlterar = pessoas[index].Id
+            break
+        }
+    }
+}
+
+function alterar(p) {
+    var btn = document.getElementById('btnCadastrarSalvar')
+
+    pessoas = JSON.parse(localStorage.getItem('pessoa'))
+        //
+    for (var index = 0; index < pessoas.length; index++) {
+
+        if (pessoas[index].Id == idAlterar) {
+
+            pessoas[index].Nome = p.nome
+            pessoas[index].DataNascimento = p.nasc.toLocaleString('pt-br').substring(0, 10)
+            pessoas[index].Sexo = p.sexo
+            pessoas[index].DataCadastro = p.data.toLocaleString('pt-br')
+
+            btn.value = 'Cadastrar'
+            idAlterar = null
+
+            localStorage.setItem('pessoa', JSON.stringify(pessoas))
+            document.getElementById('frmCadastro').reset()
+
+            break
+        }
+    }
+}
+
+// BLOCO EXCLUIR
+function excluir(codigo) {
+
+    // busca os dados no localstorage
+    var pessoas = JSON.parse(localStorage.getItem('pessoa'))
+
+    for (var index = 0; index < pessoas.length; index++)
+        if (pessoas[index].Id = codigo)
+            pessoas.splice(index, 1)
+
+    localStorage.setItem('pessoa', JSON.stringify(pessoas))
+
+    listar()
+
+    // se não houver nenhum registro, limpar o storage
+    if (pessoas.length == 0)
+        window.localStorage.removeItem('pessoa')
+
 }
